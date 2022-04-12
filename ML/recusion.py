47a9ef9f4267d99fe6777fd1298c315586811c7b
Ahmed Awad <33605748+ahmedhawad@users.noreply.github.com>
2022-04-12 12:45:38 -0400

    Adding AI Work

---
+++ ML/recusion.py
@@ -0,0 +1,43 @@
+
+
+
+# Recurison is very import to understanding AI
+
+
+
+
+def recursion(num, end_num , skipby = 1):
+
+
+    #printing withhout exiting ur recursion will make u keep going
+
+    pass
+
+def return_recursion(num, end_num , skipby = 1):
+
+    #   returning breaks recursion but if printing u can't get the output
+
+    pass
+
+
+
+
+
+def looping(num, end_num , skipby = 1):
+
+    # Recurison is very import to understanding AI
+
+
+    pass
+
+
+
+
+def start_application(current_node,Exit = False):
+    #you can think of it like starting an application or a while loop
+
+    pass
+
+
+
+
